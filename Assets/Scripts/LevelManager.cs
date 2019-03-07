4d19dfddb261ee0ec25521ba794d05e0f627de98
cyborg42 <isliumiao@outlook.com>
2019-03-07 21:29:21 +0800

    add sprites

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject floor1, floor2;
+
+}
