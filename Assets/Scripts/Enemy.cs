14c6b2f55704b7cfd47085ab398bb85d3d6a6e4d
cyborg42 <isliumiao@outlook.com>
2019-03-10 21:26:38 +0800

    tower&enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 1;
+    public float blood = 100;
+    public float randomRange = (float)0.5;
+    private LevelManager levelScript;
+    private bool lck;
+    private int x, y;
+    private int lastx, lasty;
+    private int dirx, diry;
+    private int lastdirx, lastdiry;
+    private float randomx, randomy;
+    private void Awake()
+    {
+        levelScript = GameObject.Find("GameManager").GetComponent<LevelManager>();
+    }
+    private void Start()
+    {
+        lck = true;
+        lastx = System.Convert.ToInt32(transform.position.x);
+        lasty = System.Convert.ToInt32(transform.position.y);
+
+    }
+    private void FixedUpdate()
+    {
+        x = System.Convert.ToInt32(transform.position.x);
+        y = System.Convert.ToInt32(transform.position.y);
+        dirx = levelScript.direction[x, y, 0];
+        diry = levelScript.direction[x, y, 1];
+        if (x == lastx && y == lasty)
+        {
+            if(lastdirx!=dirx||lastdiry!=diry)
+            {
+                lck = false;
+            }
+            if (lck)
+            {
+                Vector3 dir = new Vector3(x + dirx - transform.position.x, y + diry - transform.position.y, 0);
+                transform.Translate(dir.normalized * speed * Time.fixedDeltaTime);
+            }
+            else
+            {
+                Vector3 dir = new Vector3(x - transform.position.x, y - transform.position.y, 0);
+                transform.Translate(dir.normalized * speed * Time.fixedDeltaTime);
+                if (dir.magnitude < 0.1)
+                {
+                    transform.position = new Vector3(x, y, 0);    
+                    lck = true;
+                }
+            }
+        }
+        else
+        {
+            lck = false;
+        }
+        lastx = x;
+        lasty = y;
+        lastdirx = dirx;
+        lastdiry = diry;
+    }
+
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Tower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float range = (float)5;
+    private List<GameObject> enemys = new List<GameObject>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        enemys.Add(collision.gameObject);
+        Debug.Log("enter");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        enemys.Remove(collision.gameObject);
+        Debug.Log("exit");
+    }
+
+    void Update()
+    {
+        if (enemys.Count == 0)
+        {
+            return;
+        }
+        while(!enemys[0])
+        {
+            enemys.RemoveAt(0);
+        }
+        if (enemys.Count == 0)
+        {
+            return;
+        }
+        Debug.Log(enemys[0].name);
+    }
+}
